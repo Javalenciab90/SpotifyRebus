6ecfb2846f3cf7f181ff4f9283bc542fb11cefa4
Jaime Valencia <javalenciab90@gmail.com>
2022-06-22 22:13:32 -0500

    set fragments

---
+++ app/src/main/java/com/java90apps/spotifyrebus/ui/fragments/HomeFragment.kt
@@ -0,0 +1,14 @@
+package com.java90apps.spotifyrebus.ui.fragments
+
+import android.os.Bundle
+import android.view.View
+import com.java90apps.spotifyrebus.databinding.FragmentHomeBinding
+import com.java90apps.spotifyrebus.domain.base.BaseFragment
+
+class HomeFragment: BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
