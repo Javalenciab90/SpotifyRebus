bc718fe7082978ccc487b80d6e308a2851d3b4a1
Jaime Valencia <javalenciab90@gmail.com>
2022-06-22 22:57:03 -0500

    setUp navigation app

--- app/src/main/java/com/java90apps/spotifyrebus/MainActivity.kt
+++ app/src/main/java/com/java90apps/spotifyrebus/MainActivity.kt
@@ -2,13 +2,53 @@ package com.java90apps.spotifyrebus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.java90apps.spotifyrebus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.homeFragment)
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onResume() {
+        handleBackPress()
+        super.onResume()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    private fun handleBackPress() = this.onBackPressedDispatcher.addCallback(object :
+        OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+
+            navController.currentDestination?.let { current ->
+                when (current.id) {
+                    R.id.homeFragment -> finish()
+                    else ->  navController.navigateUp()
+                }
+            }
+        }
+    })
+
 }
