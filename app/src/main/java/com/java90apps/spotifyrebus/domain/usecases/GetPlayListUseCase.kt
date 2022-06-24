c031e2d2eb2ef1a3b3bd45ba7859e828283fede2
Jaime Valencia <javalenciab90@gmail.com>
2022-06-23 21:39:19 -0500

    set UseCases

--- app/src/main/java/com/java90apps/spotifyrebus/domain/usecases/GetPlayListUseCase.kt
+++ app/src/main/java/com/java90apps/spotifyrebus/domain/usecases/GetPlayListUseCase.kt
@@ -1,10 +1,13 @@
 package com.java90apps.spotifyrebus.domain.usecases
 
+import com.java90apps.spotifyrebus.domain.models.PlayListModel
 import com.java90apps.spotifyrebus.domain.repository.Repository
 import javax.inject.Inject
 
 class GetPlayListUseCase @Inject constructor(
     private val repository: Repository
 ) {
-
+    suspend operator fun invoke(channelId: Double) : PlayListModel {
+        return repository.getPlayListChannel(channelId)
+    }
 }
